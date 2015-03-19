2d3c8f06ce4dc21e504d08f1c878f8088c00f31d
mleah <mariah.hanson@detroitlabs.com>
2015-03-19 14:17:51 -0400

    fixing loop and checker, adding check to see if 13 digits.  no other error handling yet

--- assessmentpractice3.js
+++ assessmentpractice3.js
@@ -17,28 +17,27 @@ ISBN-13 is made up of 12 digits plus a check digit. Spaces and hyphens may be in
 The check digit for ISBN-13 is calculated by multiplying each digit alternately by 1 or 3 (i.e., 1 x 1st digit, 3 x 2nd digit, 1 x 3rd digit, 3 x 4th digit, etc.), summing these products together, taking modulo 10 of the result and subtracting this value from 10, and then taking the modulo 10 of the result again to produce a single digit.
 
 Create a function that takes a string and returns true if that is a valid ISBN-13 and false otherwise.
-
-PSEDUOCODE
-
-ok, looks like I need regex for spaces and hypens.  should use .replace for the string
-
 */
+
 var sget = require("sget");
 
 
 function checkISBN(isbn){
-
 	var isbnNumber = userInput(isbn); 
+
+	if(isbnNumber.length === 13){
 	
-	if(isValidISBN(isbnNumber)){
-		console.log("\n" + isbnNumber + " is a valid ISBN!\n");
+		if(isValidISBN(isbnNumber)){
+			console.log("\n" + isbnNumber + " is a valid ISBN!\n");
+		} else {
+			console.log("\n" + isbnNumber + " is not a valid ISBN!\n");
+		}
 	} else {
-		console.log("\n" + isbnNumber + " is not a valid ISBN!\n");
+		console.log("That is not an ISBN-13.")
 	}
 }
 
-
-function userInput(isbnInput){ // for future reference if I want to get userInput eventually
+function userInput(isbnInput){
 
 	var isbnNumberInput = isbnInput.replace(/\s+|-+/g, "");
 	return isbnNumberInput;
@@ -46,8 +45,7 @@ function userInput(isbnInput){ // for future reference if I want to get userInpu
 }
 
 
-function isValidISBN(isbn){  //turn into number, turn into array, apply specific math against even or odd indexes of array
-	
+function isValidISBN(isbn){ 
 	var isbnArray = isbn.split("").map(Number);
 
 	var isbnCounter = 0;
@@ -55,23 +53,16 @@ function isValidISBN(isbn){  //turn into number, turn into array, apply specific
 
 
 	for(var i = 0; i < isbnArray.length; i++){
-		if(isbnArray[i] === 0 || isbnArray[i] % 2 === 0){
+		if(i === 0 || i % 2 === 0){
 			isbnCounter += isbnArray[i];
-			console.log(isbnCounter);
-			
 		} else {
-			var multiplyByThree = isbnArray[i] * 3;
-			isbnCounter +=multiplyByThree;
-			console.log(isbnCounter);
-			
+			isbnCounter +=isbnArray[i] * 3;	
 		}
 	}
 
 	var isbnChecker = (10 - isbnCounter%10)%10;
 
-	console.log("This is the ISBN counter.  " + isbnCounter + " This is the ISBN checker.  " + isbnChecker + "  This is the isbn digitchecker: " + isbnCheckDigit);
-
-	if(isbnCounter === isbnCheckDigit){
+	if(isbnCheckDigit === isbnChecker){
 		return true;
 	} else{
 		return false;
@@ -80,5 +71,10 @@ function isValidISBN(isbn){  //turn into number, turn into array, apply specific
 }
 
 
-checkISBN("978   0470059--029");
+checkISBN("978-0-13-149505-0");
+checkISBN("9780470059029");
+checkISBN("978-0-13-149505-0");
+checkISBN("978-0-262-13472-9");
+checkISBN("2920109293847");
+checkISBN("29201092938");
 
